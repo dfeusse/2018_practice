6a2269382190b1d078224694d24ddfc4570a0a62
Daniel Feusse <danielfeusse@Daniels-MacBook-Air.local>
2018-05-14 13:20:58 -0700

    add all files

--- dailyPython/05_may/14_iqTest.py
+++ dailyPython/05_may/14_iqTest.py
@@ -24,3 +24,11 @@ def iq_test(string):
 print iq_test("2 4 7 8 10")# => 3 // Third number is odd, while the rest of the numbers are even
 
 print iq_test("1 2 1 1")
+
+
+'''
+def iq_test(numbers):
+    e = [int(i) % 2 == 0 for i in numbers.split()]
+
+    return e.index(True) + 1 if e.count(True) == 1 else e.index(False) + 1
+    '''
